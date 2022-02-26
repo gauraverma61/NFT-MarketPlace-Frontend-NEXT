ab21993176ec14ecd61e6d6fae319a668e28e6b3
gaurav.verma <gauraverma.work@gmail.com>
2022-02-26 14:37:12 +0530

    complet footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,4 +1,59 @@
+import MainIcon from "@atoms/MainIcon";
+import Link from "next/link";
 import React from "react";
+
+const linksData = [
+  {
+    title: "MarketPlace",
+    data: [
+      "All NFTs",
+      "Art",
+      "Collectibles",
+      "Domain Names",
+      "Music",
+      "Photography",
+      "Sports",
+      "Trading Cards",
+      "Utility",
+      "Virtual Worlds",
+    ],
+  },
+  {
+    title: "My Account",
+    data: ["Profile", "favorites", "Watchlist", "My Collections", "Settings"],
+  },
+  {
+    title: "Resources",
+    data: [
+      "Help Center",
+      "Platform Status",
+      "Partners",
+      "Gas Fee Marketplaces",
+      "Taxes",
+      "Blog",
+      "Docs",
+      "Newsletter",
+    ],
+  },
+  { title: "Company", data: ["About", "Careers", "Ventures", "Grants"] },
+];
+
+const Linkbox = (props: any) => {
+  const { title, data } = props;
+  return (
+    <div className="link_box col-12 col-md-4 col-lg-3">
+      <h5>{title}</h5>
+      {data.map((link: any, index: any) => {
+        return (
+          // <Link href="">
+          <p className="link" key={index}>
+            {link}
+          </p>
+        );
+      })}
+    </div>
+  );
+};
 
 const Footer = () => {
   return (
@@ -45,7 +100,41 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        
+        <div className="links_section">
+          <div className="row">
+            <div className="left_section d-flex align-items-left col-12 col-lg-3">
+              <MainIcon />
+              <h4>Sasible</h4>
+              <p>
+                The world’s first and largest digital marketplace for crypto
+                collectibles and non-fungible tokens (NFTs). Buy, sell, and
+                discover exclusive digital items.
+              </p>
+            </div>
+            <div className="right_section col-12 col-lg-9">
+              <div className="row">
+                {linksData.map((linkdata, index) => {
+                  return (
+                    <Linkbox
+                      key={index}
+                      title={linkdata.title}
+                      data={linkdata.data}
+                    />
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="bottom_footer d-flex aling-items-center justify-content-between">
+          <div className="left">© 2018 - 2022 Ozone Networks, Inc</div>
+          <div className="right">
+            <ul className="d-flex align-items-center">
+              <div>Privacy Policy</div>
+              <div className="ms-3">Terms of Service</div>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
